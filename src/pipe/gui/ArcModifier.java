9b4227d733ff4f305a3a1aaa0ddfd2bdd415aac7
Paul Molina <paulrmolina@gmail.com>
2018-04-10 19:49:22 -0400

    Add all Arc fields and implement reset functionality

--- src/pipe/gui/ArcModifier.java
+++ src/pipe/gui/ArcModifier.java
@@ -13,17 +13,25 @@ import java.util.Vector;
 
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ArcModifier extends JPanel {
 	private Arc currentArc;
 	private JTextField txtName;
-	private JTextField textField_1;
+	private JTextField txtVariable;
+	private JTextField txtWeight;
+	
+	private String originalDataType, originalName, originalVariableValue;
+	private int originalWeight;
 	
 	public ArcModifier(Arc anArc) {
 		setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(0, 0, 400, 380);
+		tabbedPane.setBounds(0, 0, 400, 300);
 		add(tabbedPane);
 		
 		JPanel ArcEditor = new JPanel();
@@ -44,41 +52,96 @@ public class ArcModifier extends JPanel {
 		separator.setBounds(10, 27, 375, 2);
 		ArcEditor.add(separator);
 		
-		// HANDLE DATA 
-		currentArc = anArc;
-		setDataType(lblDataType);
-		
-		
-		// END HANDLE DATA
-		
 		JLabel lblName = new JLabel("Name:");
 		lblName.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblName.setBounds(10, 81, 57, 23);
+		lblName.setBounds(10, 64, 57, 23);
 		ArcEditor.add(lblName);
 		
 		JLabel lblVariable_1 = new JLabel("Variable:");
 		lblVariable_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblVariable_1.setBounds(10, 122, 57, 23);
+		lblVariable_1.setBounds(10, 95, 57, 23);
 		ArcEditor.add(lblVariable_1);
 		
 		txtName = new JTextField();
-		txtName.setBounds(70, 84, 262, 20);
+		txtName.setBounds(70, 67, 315, 20);
 		ArcEditor.add(txtName);
 		txtName.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(70, 125, 262, 20);
-		ArcEditor.add(textField_1);
+		txtVariable = new JTextField();
+		txtVariable.setColumns(10);
+		txtVariable.setBounds(70, 98, 315, 20);
+		ArcEditor.add(txtVariable);
 		
 		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(10, 166, 375, 2);
+		separator_1.setBounds(10, 141, 375, 2);
 		ArcEditor.add(separator_1);
+		
+
+		
+		JLabel lblArcHeaderName = new JLabel("ARC_NAME");
+		lblArcHeaderName.setFont(new Font("Monospaced", Font.PLAIN, 14));
+		lblArcHeaderName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblArcHeaderName.setBounds(10, 2, 375, 24);
+		ArcEditor.add(lblArcHeaderName);
+		
+		txtWeight = new JTextField();
+		txtWeight.setText((String) null);
+		txtWeight.setColumns(10);
+		txtWeight.setBounds(70, 157, 315, 20);
+		ArcEditor.add(txtWeight);
+		
+		JLabel lblWeight = new JLabel("Weight:");
+		lblWeight.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblWeight.setBounds(10, 154, 57, 23);
+		ArcEditor.add(lblWeight);
+		
+		
+		
+		JButton btnOK = new JButton("OK");
+		btnOK.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+			}
+		});
+		btnOK.setBounds(40, 198, 144, 36);
+		ArcEditor.add(btnOK);
+		
+		
+		
+		
+		
+		JButton btnReset = new JButton("Reset");
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblDataType.setText(originalDataType);
+				txtName.setText(originalName); 
+				txtVariable.setText(originalVariableValue);
+				txtWeight.setText("" + originalWeight);
+			}
+		});
+		btnReset.setBounds(229, 198, 144, 36);
+		ArcEditor.add(btnReset);
+		
+		
+		// HANDLE DATA 
+		currentArc = anArc;
+		setDataType(lblDataType);
+		setName(txtName);
+		setVariable(txtVariable);
+		setWeight(txtWeight);
+		setHeaderName(lblArcHeaderName);
+		// END HANDLE DATA
+		
+
 
 	}
 	
 
-	
+	/**
+	 * Changes Data Type label
+	 * @param aDataType
+	 */
 	private void setDataType(JLabel aDataType)
 	{
 	  DataType d = currentArc.getDataType();
@@ -101,17 +164,42 @@ public class ArcModifier extends JPanel {
 				  s += " >)";
 			  else s += " >";
 			  aDataType.setText(s);
+			  
+			  originalDataType = s;
 		   
 	}
 	}
 	
+	private void setHeaderName(JLabel aHeaderName)
+	{
+		aHeaderName.setText(currentArc.getName());
+	}
+	
 	/**
-	 * Loads all of the relevant data from a selected arc onto the form
+	 * Changes Name text field
 	 * @param anArc
 	 */
 	private void setName(JTextField aNameField)
 	{
-
+		originalName = currentArc.getName();
 		aNameField.setText(currentArc.getName());
+		
+	}
+	
+	/**
+	 * Changes Variable text field
+	 * @param aVariableField
+	 */
+	private void setVariable(JTextField aVariableField)
+	{
+		originalVariableValue = currentArc.getVar();
+		aVariableField.setText(currentArc.getVar());
+	}
+	
+	
+	private void setWeight(JTextField aWeightField)
+	{
+		originalWeight = currentArc.getWeight();
+		aWeightField.setText("" + currentArc.getWeight());
 	}
 }
